a1a901153c5858f1f6360d84233850a9ec18aab9
muhammadalizaryani-a11y <muhammadalizaryani@gmail.com>
2026-08-05 19:56:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
+// MA DIGITAL STORE AI STUDIO
 
-    const button = document.querySelector("button");
+const button = document.querySelector("button");
 
-    button.addEventListener("click", function () {
-
-        alert("Welcome to MA DIGITAL STORE AI STUDIO");
-
-    });
-
+button.addEventListener("click", function () {
+    alert("Welcome to MA DIGITAL STORE AI STUDIO 🚀");
 });
